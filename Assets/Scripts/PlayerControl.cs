6663513ee10b9bfbf93d3d549a628d5aac56b0f8
Secretary-bird <LLopez31098@gmail.com>
2020-10-01 23:05:04 -0400

    maze building and player movement

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+	public float speed;
+
+	private float movementX;
+	private float movementY;
+
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		print(movementX + "   " + movementY);
+		Vector3 movement = new Vector3(0.0f, movementY, movementX);
+
+		rb.AddForce(movement * speed);
+
+	}
+
+
+	public void OnMove(InputValue value)
+	{
+		Vector2 v = value.Get<Vector2>();
+
+		movementX = v.x;
+		movementY = v.y;
+	}
+
+}
