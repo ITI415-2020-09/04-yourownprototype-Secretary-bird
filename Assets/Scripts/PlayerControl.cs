448c3d9052030ce831f3b7d4f10ee1639e0ce1aa
Secretary-bird <LLopez31098@gmail.com>
2020-10-08 23:09:52 -0400

    fixed rotation movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -11,17 +11,41 @@ public class PlayerControl : MonoBehaviour
 	private float movementY;
 	private Rigidbody rb;
 	private Vector3 movement;
+	private int faceNum;
 
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody>();
+		rb.constraints = RigidbodyConstraints.FreezePositionX;
+		faceNum = 1;
 	}
 
 	void FixedUpdate()
 	{
 		print(movementX + "   " + movementY);
-		movement = new Vector3(0.0f, movementY, movementX);
+        switch (faceNum)
+        {
+			case 1:
+				movement = new Vector3(0.0f, movementY, movementX);
+				break;
+			case 2:
+				movement = new Vector3(movementX, movementY, 0.0f);
+				break;
+			case 3:
+				movement = new Vector3(0.0f, movementY, -movementX);
+				break;
+			case 4:
+				movement = new Vector3(-movementX, 0.0f, movementY);
+				break;
+			case 5:
+				movement = new Vector3(-movementX, movementY, 0.0f);
+				break;
+			case 6:
+				movement = new Vector3(-movementX, 0.0f, -movementY);
+				break;
+		}
+
 		rb.AddForce(movement * speed);
 
 	}
@@ -39,30 +63,34 @@ public class PlayerControl : MonoBehaviour
 		if (other.gameObject.CompareTag("Stage_1"))
 		{
 			rb.velocity = new Vector3(0, 0, 0);
+			faceNum = 2;
+			rb.constraints = RigidbodyConstraints.FreezePositionZ;
 			transform.position = new Vector3(13f, 23.1f, -15.5f);
 			CameraOrb.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
-			movement = new Vector3(movementX, movementY, 0.0f);
 		}
 		if (other.gameObject.CompareTag("Stage_2"))
 		{
 			rb.velocity = new Vector3(0, 0, 0);
+			faceNum = 3;
+			rb.constraints = RigidbodyConstraints.FreezePositionX;
 			transform.position = new Vector3(-15.5f, 18.3f, -13f);
 			CameraOrb.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
-			movement = new Vector3(0.0f, movementY, -movementX);
 		}
 		if (other.gameObject.CompareTag("Stage_3"))
 		{
 			rb.velocity = new Vector3(0, 0, 0);
+			faceNum = 4;
+			rb.constraints = RigidbodyConstraints.FreezePositionY;
 			transform.position = new Vector3(-12.5f, 4.5f, -12.5f);
 			CameraOrb.transform.Rotate(0.0f, 90.0f, -90.0f, Space.Self);
-			movement = new Vector3(-movementX, 0.0f, movementY);
 		}
 		if (other.gameObject.CompareTag("Stage_4"))
 		{
 			rb.velocity = new Vector3(0, 0, 0);
+			faceNum = 5;
+			rb.constraints = RigidbodyConstraints.FreezePositionZ;
 			transform.position = new Vector3(11.5f, 8f, 15.5f);
 			CameraOrb.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
-			movement = new Vector3(-movementX, movementY, 0.0f);
 		}
 		if (other.gameObject.CompareTag("Death"))
 		{
@@ -72,9 +100,10 @@ public class PlayerControl : MonoBehaviour
 		if (other.gameObject.CompareTag("Stage_5"))
 		{
 			rb.velocity = new Vector3(0, 0, 0);
+			faceNum = 6;
+			rb.constraints = RigidbodyConstraints.FreezePositionY;
 			transform.position = new Vector3(5.5f, 35.5f, 13.5f);
 			CameraOrb.transform.Rotate(0.0f, 0.0f, 90.0f, Space.Self);
-			movement = new Vector3(-movementX, 0.0f, -movementY);
 		}
 
 	}
